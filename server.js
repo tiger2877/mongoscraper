092bad53789b6c3cc3ea77201533b3a8c68c6aa3
tiger2877 <pcahyadi@gmail.com>
2019-03-28 18:25:23 -0400

    I made some changes

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.nytimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("balancedHeadline").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
